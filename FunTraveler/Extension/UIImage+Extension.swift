1383423abbb56465ac698c3a77f13526ea2ed5b9
sallylin <missikun.tw@gmail.com>
2022-04-10 22:05:36 +0800

    Add Extension to simply the code

---
+++ FunTraveler/Extension/UIImage+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+Extension.swift
+//  FunTraveler
+//
+//  Created by 林翊婷 on 2022/4/9.
+//
+
+import Foundation
+import UIKit
+
+enum ImageAsset: String {
+
+    // Planning Page
+    case orderMarker
+    case zoomIn
+    case zoomOut
+
+}
+
+extension UIImage {
+
+    static func asset(_ asset: ImageAsset) -> UIImage? {
+
+        return UIImage(named: asset.rawValue)
+    }
+}
