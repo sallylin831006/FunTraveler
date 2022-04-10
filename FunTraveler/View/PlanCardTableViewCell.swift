63baa3cc0507c6a69a698a06da4deb24a3e86dad
sallylin <missikun.tw@gmail.com>
2022-04-10 21:57:45 +0800

    Manage time calculation with TimeManager and timePicker

--- FunTraveler/View/PlanCardTableViewCell.swift
+++ FunTraveler/View/PlanCardTableViewCell.swift
@@ -8,6 +8,10 @@
 import UIKit
 class PlanCardTableViewCell: UITableViewCell {
 
+    var durationTime: String?
+
+    var times = ["1", "1.5", "2", "2.5", "3", "3.5"]
+
     @IBOutlet weak var pickerView: TimePickerView!
     
     @IBOutlet weak var nameLabel: UILabel!
@@ -22,6 +26,25 @@ class PlanCardTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+    func layoutCell(
+        startTime: String
+    ) {
+        
+        startTimeLabel.text = startTime
+        
+        do {
+            let date = try TimeManager.getDateFromString(
+                dateFormat: "HH:mm", dateString: startTime, duration:
+                    Double(pickerView.timeTextField.text ?? "2.00") ?? 2.00)
+            
+            endTimeLabel.text = "\(date.endHours):\(date.endMinutes)"
+
+        } catch let wrongError {
+            print("Error message: \(wrongError),Please add correct time!")
+        }
+        
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
