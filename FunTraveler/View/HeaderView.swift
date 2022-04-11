fcbfe9d382c9944de832d25f009256defe7a1b9a
sallylin <missikun.tw@gmail.com>
2022-04-11 11:58:49 +0800

    Add header and footer view and cell xib for AddPlanViewController

---
+++ FunTraveler/View/HeaderView.swift
@@ -0,0 +1,32 @@
+//
+//  HeaderView.swift
+//  FunTraveler
+//
+//  Created by 林翊婷 on 2022/4/11.
+//
+
+import UIKit
+
+class HeaderView: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+   
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        // -------改不動-------//
+        setupHeaderView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupHeaderView()
+    }
+
+    private func setupHeaderView() {
+        contentView.backgroundColor = UIColor(patternImage: UIImage.asset(.headerBackgroundImage)!)
+        
+    }
+
+}
