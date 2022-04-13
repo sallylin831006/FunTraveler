0483798dc55270fbecd0c00973dd6c337aa94718
sallylin <missikun.tw@gmail.com>
2022-04-14 00:20:24 +0800

    Auto generated files

--- FunTraveler/AppDelegate.swift
+++ FunTraveler/AppDelegate.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.enable = true
         // Override point for customization after application launch.
         return true
     }
