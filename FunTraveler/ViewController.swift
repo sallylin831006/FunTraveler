bf1a55dc68ae905409d653aee001fae177ab94b2
sallylin <missikun.tw@gmail.com>
2022-04-18 14:14:57 +0800

    stash

---
+++ FunTraveler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunTraveler
+//
+//  Created by 林翊婷 on 2022/4/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
