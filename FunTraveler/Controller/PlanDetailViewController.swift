f7120189adc3cbcf7a245f3fcbf12254e75d93b7
sallylin <missikun.tw@gmail.com>
2022-04-19 10:49:50 +0800

    Fixed bugger for days

--- FunTraveler/Controller/PlanDetailViewController.swift
+++ FunTraveler/Controller/PlanDetailViewController.swift
@@ -31,7 +31,7 @@ class PlanDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addMap()
+        //addMap()
         showPlanPicker()
         addCustomBackButton()
         
