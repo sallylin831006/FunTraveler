3307135587ec884e8d69e716e802375ffcade3d3
sallylin <missikun.tw@gmail.com>
2022-04-24 12:27:43 +0800

    FUnctionally Get Video url from API

---
+++ FunTraveler/Model/Video/VideoObject.swift
@@ -0,0 +1,8 @@
+//
+//  VideoObject.swift
+//  FunTraveler
+//
+//  Created by 林翊婷 on 2022/4/24.
+//
+
+import Foundation
---
+++ FunTraveler/Model/Video/VideoRequest.swift
@@ -0,0 +1,8 @@
+//
+//  VideoRequest.swift
+//  FunTraveler
+//
+//  Created by 林翊婷 on 2022/4/24.
+//
+
+import Foundation
