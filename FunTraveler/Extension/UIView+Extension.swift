f4f0e7dbce61ffbe3b0c327506fa2d771be8a09a
sallylin <missikun.tw@gmail.com>
2022-04-24 12:27:43 +0800

    FUnctionally Get Video url from API

---
+++ FunTraveler/Extension/UIView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extension.swift
+//  FunTraveler
+//
+//  Created by 林翊婷 on 2022/4/24.
+//
+
+import Foundation
---
+++ FunTraveler/Model/Video/VideoProvider.swift
@@ -0,0 +1,8 @@
+//
+//  VideoProvider.swift
+//  FunTraveler
+//
+//  Created by 林翊婷 on 2022/4/24.
+//
+
+import Foundation
