78f4966249ebf0138e9aaeae708fdd7e5f1d9a32
sallylin <missikun.tw@gmail.com>
2022-05-11 11:52:19 +0800

    Adjust UI setting for PlanPicker

--- FunTraveler/View/TimePickerView.swift
+++ FunTraveler/View/TimePickerView.swift
@@ -40,7 +40,8 @@ extension TimePickerView {
         let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 35))
         toolBar.barStyle = UIBarStyle.default
         toolBar.isTranslucent = true
-        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+        toolBar.tintColor = .themeLightBlue
+//        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
         toolBar.sizeToFit()
 
         let doneButton = UIBarButtonItem(
